052a2528fff3029e39b5054116b7aa70a649188c
jervinjosh-gcash <jervinjosh.valencia@gcash.com>
2024-07-31 14:15:45 +0800

    added dependencies and pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App />
+  },
+  {
+    path:'/abc',
+    element: <h1>PAGE ABC</h1>
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
